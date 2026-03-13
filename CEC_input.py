50f5388879d9c78b3817d9612158e3c6b1219df9
Bender <bender@VOID.lan>
2026-03-12 23:49:29 -0500

    Update CEC_input.py

--- CEC_input.py
+++ CEC_input.py
@@ -6,6 +6,7 @@ import uinput
 # Blue = 113 Red = 114 Green = 115 Yellow = 116
 # Ok = 0 Back = 13
 # up = 1 down = 2 left = 3 right = 4
+
 KEY_MAP = {
     0: [uinput.KEY_ENTER],
     1: [uinput.KEY_UP],
